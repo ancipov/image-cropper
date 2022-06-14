131f6ca809048d1ca85573e9f2b6ef18b3581587
aancipov <aancipov@exadel.com>
2022-06-14 14:01:25 +0300

    update

--- projects/demo-app/src/app/app.component.ts
+++ projects/demo-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Dimensions, ImageCroppedEvent, ImageTransform } from 'ngx-image-cropper';
+import { Dimensions, ImageCroppedEvent, ImageTransform } from 'ngx-image-cropper-workspace';
 
 @Component({
   selector: 'app-root',
